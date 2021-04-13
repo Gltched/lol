719765a64e81d624752657538fb5700bbdc31c37
Gltched <gltchcraftmc@gmail.com>
2021-04-13 13:35:20 -0700

    3 files modified, 6 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import "./style.css";
 
-export default function App() {
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
-}
+};
+
+export default App;
